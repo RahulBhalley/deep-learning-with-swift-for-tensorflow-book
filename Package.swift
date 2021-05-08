95746c8f5503c892aaadbc216077e5d21d7d2040
Rahul Bhalley <rshulbhalley@icloud.com>
2021-05-08 07:00:46 +0530

    re-formatted Package.swift

--- Package.swift
+++ Package.swift
@@ -16,133 +16,105 @@ let package = Package(
         
         // MARK: Chapter 3: Differentiable Programming
         
-        .target(
-            name: "AdvancedOperators",
-            path: "Sources/DifferentiableProgramming/AdvancedOperators"),
+        .target(name: "AdvancedOperators",
+                path: "Sources/DifferentiableProgramming/AdvancedOperators"),
         
-        .target(
-            name: "AlgorithmicDifferentiation",
-            path: "Sources/DifferentiableProgramming/AlgorithmicDifferentiation"),
+        .target(name: "AlgorithmicDifferentiation",
+                path: "Sources/DifferentiableProgramming/AlgorithmicDifferentiation"),
         
-        .target(
-            name: "Arrays",
-            path: "Sources/DifferentiableProgramming/Arrays"),
+        .target(name: "Arrays",
+                path: "Sources/DifferentiableProgramming/Arrays"),
         
-        .target(
-            name: "Classes",
-            path: "Sources/DifferentiableProgramming/Classes"),
+        .target(name: "Classes",
+                path: "Sources/DifferentiableProgramming/Classes"),
         
-        .target(
-            name: "Closures",
-            path: "Sources/DifferentiableProgramming/Closures"),
+        .target(name: "Closures",
+                path: "Sources/DifferentiableProgramming/Closures"),
         
-        .target(
-            name: "ConditionalStatements",
-            path: "Sources/DifferentiableProgramming/ConditionalStatements"),
+        .target(name: "ConditionalStatements",
+                path: "Sources/DifferentiableProgramming/ConditionalStatements"),
         
-        .target(
-            name: "ControlTransfer",
-            path: "Sources/DifferentiableProgramming/ControlTransfer"),
+        .target(name: "ControlTransfer",
+                path: "Sources/DifferentiableProgramming/ControlTransfer"),
         
-        .target(
-            name: "Dictionaries",
-            path: "Sources/DifferentiableProgramming/Dictionaries"),
+        .target(name: "Dictionaries",
+                path: "Sources/DifferentiableProgramming/Dictionaries"),
         
-        .target(
-            name: "Differentiation",
-            path: "Sources/DifferentiableProgramming/Differentiation"),
+        .target(name: "Differentiation",
+                path: "Sources/DifferentiableProgramming/Differentiation"),
         
-        .target(
-            name: "EarlyExit",
-            path: "Sources/DifferentiableProgramming/EarlyExit"),
+        .target(name: "EarlyExit",
+                path: "Sources/DifferentiableProgramming/EarlyExit"),
         
-        .target(
-            name: "Enumerations",
-            path: "Sources/DifferentiableProgramming/Enumerations"),
+        .target(name: "Enumerations",
+                path: "Sources/DifferentiableProgramming/Enumerations"),
         
-        .target(
-            name: "ErrorHandling",
-            path: "Sources/DifferentiableProgramming/ErrorHandling"),
+        .target(name: "ErrorHandling",
+                path: "Sources/DifferentiableProgramming/ErrorHandling"),
         
-        .target(
-            name: "Extensions",
-            path: "Sources/DifferentiableProgramming/Extensions"),
+        .target(name: "Extensions",
+                path: "Sources/DifferentiableProgramming/Extensions"),
         
-        .target(
-            name: "Generics",
-            path: "Sources/DifferentiableProgramming/Generics"),
+        .target(name: "Generics",
+                path: "Sources/DifferentiableProgramming/Generics"),
         
-        .target(
-            name: "GlobalFunctions",
-            path: "Sources/DifferentiableProgramming/GlobalFunctions"),
+        .target(name: "GlobalFunctions",
+                path: "Sources/DifferentiableProgramming/GlobalFunctions"),
         
-        .target(
-            name: "Loops",
-            path: "Sources/DifferentiableProgramming/Loops"),
+        .target(name: "Loops",
+                path: "Sources/DifferentiableProgramming/Loops"),
         
-        .target(
-            name: "NestedFunctions",
-            path: "Sources/DifferentiableProgramming/NestedFunctions"),
+        .target(name: "NestedFunctions",
+                path: "Sources/DifferentiableProgramming/NestedFunctions"),
         
-        .target(
-            name: "Protocols",
-            path: "Sources/DifferentiableProgramming/Protocols"),
+        .target(name: "Protocols",
+                path: "Sources/DifferentiableProgramming/Protocols"),
         
-        .target(
-            name: "PythonInteroperability",
-            path: "Sources/DifferentiableProgramming/PythonInteroperability"),
+        .target(name: "PythonInteroperability",
+                path: "Sources/DifferentiableProgramming/PythonInteroperability"),
         
-        .target(
-            name: "Sets",
-            path: "Sources/DifferentiableProgramming/Sets"),
+        .target(name: "Sets",
+                path: "Sources/DifferentiableProgramming/Sets"),
         
-        .target(
-            name: "Structures",
-            path: "Sources/DifferentiableProgramming/Structures"),
+        .target(name: "Structures",
+                path: "Sources/DifferentiableProgramming/Structures"),
         
-        .target(
-            name: "Values",
-            path: "Sources/DifferentiableProgramming/Values"),
+        .target(name: "Values",
+                path: "Sources/DifferentiableProgramming/Values"),
         
         // MARK: Chapter 4: TensorFlow Basics
         
-        .target(
-            name: "EpochAndBatches",
-            dependencies: [
-                .product(name: "Datasets", package: "swift-models")
-            ],
-            path: "Sources/TensorFlowBasics/EpochAndBatches"),
+        .target(name: "EpochAndBatches",
+                dependencies: [
+                    .product(name: "Datasets", package: "swift-models")
+                ],
+                path: "Sources/TensorFlowBasics/EpochAndBatches"),
         
-        .target(
-            name: "FromScratchForResearch",
-            dependencies: [
-                .product(name: "Datasets", package: "swift-models"),
-                .product(name: "TrainingLoop", package: "swift-models"),
-            ],
-            path: "Sources/TensorFlowBasics/FromScratchForResearch"),
+        .target(name: "FromScratchForResearch",
+                dependencies: [
+                    .product(name: "Datasets", package: "swift-models"),
+                    .product(name: "TrainingLoop", package: "swift-models"),
+                ],
+                path: "Sources/TensorFlowBasics/FromScratchForResearch"),
         
-        .target(
-            name: "ModelDefinition",
-            path: "Sources/TensorFlowBasics/ModelDefinition"),
+        .target(name: "ModelDefinition",
+                path: "Sources/TensorFlowBasics/ModelDefinition"),
         
-        .target(
-            name: "TensorExplanation",
-            path: "Sources/TensorFlowBasics/TensorExplanation"),
+        .target(name: "TensorExplanation",
+                path: "Sources/TensorFlowBasics/TensorExplanation"),
         
-        .target(
-            name: "TrainingAndTesting",
-            dependencies: [
-                .product(name: "Checkpoints", package: "swift-models"),
-                .product(name: "Datasets", package: "swift-models"),
-            ],
-            path: "Sources/TensorFlowBasics/TrainingAndTesting"),
+        .target(name: "TrainingAndTesting",
+                dependencies: [
+                    .product(name: "Checkpoints", package: "swift-models"),
+                    .product(name: "Datasets", package: "swift-models"),
+                ],
+                path: "Sources/TensorFlowBasics/TrainingAndTesting"),
         
-        .target(
-            name: "TrainingLoopExample",
-            dependencies: [
-                .product(name: "Datasets", package: "swift-models"),
-                .product(name: "TrainingLoop", package: "swift-models"),
-            ],
-            path: "Sources/TensorFlowBasics/TrainingLoopExample"),
+        .target(name: "TrainingLoopExample",
+                dependencies: [
+                    .product(name: "Datasets", package: "swift-models"),
+                    .product(name: "TrainingLoop", package: "swift-models"),
+                ],
+                path: "Sources/TensorFlowBasics/TrainingLoopExample"),
     ]
 )
